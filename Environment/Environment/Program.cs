96041e670c3305f898a620f4d863e79938c1777f
Валентин Кравченко <Rowingist@mail.ru>
2021-08-03 22:20:47 +0300

    The first homework passing

---
+++ Environment/Environment/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Environment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bot bot = new Bot();
+            Player player = new Player(100);
+
+            bot.OnSeePlayer(player);
+
+            Console.WriteLine(player.Health);
+        }
+    }
+
+    class Weapon
+    {
+        private int _damage;
+        private int _bullets;
+
+        public Weapon(int damage, int bullets)
+        {
+            _damage = damage;
+            _bullets = bullets;
+        }
+
+        public void Fire(Player player)
+        {
+            player.TakeDamage(_damage);
+            _bullets -= 1;
+        }
+    }
+
+    class Player
+    {
+        public int Health { get; private set; }
+
+        public Player(int value)
+        {
+            Health = value;
+        }
+
+        public void TakeDamage(int damage)
+        {
+            Health -= damage;
+
+            if (Health <= 0)
+                Health = 0;
+        }
+    }
+
+    class Bot
+    {
+        private Weapon _weapon = new Weapon(40, 60);
+
+        public void OnSeePlayer(Player player)
+        {
+            _weapon.Fire(player);
+        }
+    }
+}
