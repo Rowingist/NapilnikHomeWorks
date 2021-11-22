ec57d61ef4c8fee943a274892228810cde9f5db9
Valentin <rowingist@gmail.com>
2021-11-22 03:31:39 +0300

    HW Example 2

---
+++ CleanCode_2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Example_Task_2_HW
+{
+    public static int GetValidated(int a, int b, int c)
+    {
+        if (a < b)
+            return b;
+        else if (a > c)
+            return c;
+        else
+            return a;
+    }
+}
