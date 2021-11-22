eb0206248953bd6de09a45fda03f052f7973ca9f
Valentin <rowingist@gmail.com>
2021-11-22 03:39:58 +0300

    HW Example 10

---
+++ CleanCode_10.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Example_Task_10_HW
+{
+    private int _bullets;
+    private int _emptyCage = 0;
+    private int _bulletsPerShoot = 1;
+
+    public bool CanShoot() => _bullets > _emptyCage;
+
+    public void Shoot() => _bullets -= _bulletsPerShoot;
+}
